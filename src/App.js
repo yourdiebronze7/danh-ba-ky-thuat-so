a0c81d1b97b6c7ac22fe8e7ccc00ef481eda1434
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-08-14 21:03:06 -0700

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Danh Bạ Kỹ Thuật Số</h1>
+      <p>Quản lý danh bạ của bạn một cách an toàn.</p>
+    </div>
+  );
+};
+
+export default App;
